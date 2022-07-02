466d5e550456d5f86057394ea797b04ad484bfe9
Andrew Davis <atd285@gmail.com>
2022-07-02 19:18:50 -0400

    mongo connection 😅

--- app/root.tsx
+++ app/root.tsx
@@ -28,7 +28,7 @@ export default function App() {
         <Meta />
         <Links />
       </head>
-      <body className="bg-black">
+      <body>
         <Outlet />
         <ScrollRestoration />
         <Scripts />
