29d313ef0f23670a0978b4c787ffe9296759e3a2
Andrew Davis <atd285@gmail.com>
2022-07-12 12:04:15 -0400

    users rendered on sidebar component

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -4,17 +4,18 @@ import { requireUserId } from '~/utils/auth.server';
 import Layout from '~/components/layout';
 import { UserPanel } from '~/components/user-panel';
 import { getOtherUsers } from '~/utils/user.server';
+import { useLoaderData } from '@remix-run/react';
 export const loader: LoaderFunction = async ({ request, params }) => {
   const userId = await requireUserId(request);
   const users = await getOtherUsers(userId);
-  console.log(users);
   return json({ users });
 };
 export default function HomeRoute() {
+  const {users} = useLoaderData();
   return (
     <Layout>
       <div className="h-full flex">
-        <UserPanel />
+        <UserPanel users={users} />
       </div>
     </Layout>
   );
