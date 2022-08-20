1609952336b1f31b47e1aafa48199c9587b89e16
Andrew Davis <atd285@gmail.com>
2022-08-20 16:13:50 -0400

    dropdown with departments

--- app/routes/home/profile.tsx
+++ app/routes/home/profile.tsx
@@ -2,19 +2,55 @@ import { json, LoaderFunction } from "@remix-run/node";
 import { Modal } from "~/components/modal";
 import { useLoaderData } from "@remix-run/react";
 import { getUser } from "~/utils/auth.server";
+import React, { useState } from 'react';
+import FormField from '~/components/form-field';
+import { departments } from "~/utils/constants";
+import { SelectBox } from "~/components/select-box";
 
-export const loader: LoaderFunction = async ({request}) => {
+export const loader: LoaderFunction = async ({ request }) => {
   const user = await getUser(request);
-  return json({user})
+  return json({ user })
 }
 
 
-export default function profile() {
-  const {user} = useLoaderData();
+export default function ProfileSettings() {
+  const { user } = useLoaderData();
+  const [formData, setFormData] = useState({
+    firstName: user?.profile?.firstName,
+    lastName: user?.profile?.lastName,
+    department: (user?.profile?.department || 'MARKETING'),
+  })
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>, field: string) => {
+    setFormData((data) => ({ ...data, [field]: e.target.value }))
+  }
+
   return (
     <Modal isOpen={true} className="w-1/3">
       <div className="p-3">
         <h2 className="text-4xl font-semibold text-blue-600 text-center mb-4">Your profile</h2>
+        <div className="flex">
+          <div className="flex-1">
+            <form method="POST">
+              <FormField htmlFor="firstName" label="First Name" value={formData.firstName} onChange={e => handleInputChange(e, 'firstName')} />
+              <FormField htmlFor="lastName" label="Last Name" value={formData.lastName} onChange={(e) => handleInputChange(e, 'lastName')} />
+              <SelectBox
+                options={departments}
+                id="department"
+                name="department"
+                label="Department"
+                value={formData.department}
+                className="w-full rounded-xl px-3 py-2 text-gray-400"
+                onChange={e => handleInputChange(e, 'department')}
+              />
+              <div className="w-full text-right mt-4">
+                <button className="roudned-xl bg-yellow-300 font-semibold text-blue-600 px-16 py-2 transition duration-300 ease-in-out hover:bg-yellow-400 hover:-translate-y-1">
+                  Save
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
       </div>
     </Modal>
   )
